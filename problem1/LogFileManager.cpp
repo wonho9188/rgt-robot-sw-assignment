b6dd0e64955c131ae148dde9c3737ba15539c351
wonho9188 <wonho9188@gmail.com>
2025-09-11 06:22:38 +0900

    [feat] problem1: 파일 입출력 메서드 기본 구현

--- problem1/LogFileManager.cpp
+++ problem1/LogFileManager.cpp
@@ -6,19 +6,34 @@ LogFileManager::~LogFileManager() {}
 
 // 로그 파일 관리 메서드
 void LogFileManager::openLogFile(const std::string& filename) {
-
+    if (logFiles.find(filename) == logFiles.end()) {
+        logFiles[filename] = std::make_unique<std::fstream>(filename, std::ios::in | std::ios::out | std::ios::app);
+    }
 }
 
 void LogFileManager::writeLog(const std::string& filename, const std::string& message) {
-
+    auto it = logFiles.find(filename);
+    if (it != logFiles.end() && it->second && it->second->is_open()) {
+        *(it->second) << message << std::endl;
+    }
 }
 
 std::vector<std::string> LogFileManager::readLogs(const std::string& filename) {
-    return {};
+    std::vector<std::string> logs;
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line)) {
+        logs.push_back(line);
+    }
+    return logs;
 }
 
 void LogFileManager::closeLogFile(const std::string& filename) {
-
+    auto it = logFiles.find(filename);
+    if (it != logFiles.end()) {
+        it->second->close();
+        logFiles.erase(it);
+    }
 }
 
 // 복사/이동 생성자 및 대입 연산자
